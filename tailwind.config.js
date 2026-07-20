5ba3d801f20b3d38fa80d833be3bea562861d322
Soheil <soheiloscar@gmail.com>
2026-07-20 23:59:43 +0330

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "brown": {
+          100 : "#ECE0D1",
+          300 : "#DBC1AC",
+          600 : "#967259",
+          900 : "#634832"
+        }
+      },
+      boxShadow: {
+        "normal": "0px 1px 10px rgba(0, 0, 0, .1)"
+      },
+      borderRadius: {
+          "4xl": "2rem"
+      },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaDemiBold": "Dana Demibold",
+        "MorabbaLight": "Morabba Light",
+        "MorabbaMedium": "Morabba Medium",
+        "MorabbaBold": "Morabba Bold",
+      },
+      letterSpacing: {
+        "tightest": "-0.065em"
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT:"1rem", 
+          lg:"0.625rem"
+        }
+      },
+      backgroundImage: {
+        "home-mobile":"url(../images/headerBgMobile.webp)",
+        "home-desktop" : "url(../images/headerBgDesktop.webp)"
+      }
+    },
+    screens: {
+      'xxs':'370px',
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+
+  },
+
+
+
+  ], 
+}
+
